10099f99ca4d664cec263f33cca52197d4b59858
jason stocks and Michelle Morin <michelle@>
2020-01-21 09:38:07 -0800

    update logic to output selection of responses in a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
+// front-end logic:
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
     var favoriteThings = [$("input#celebrity").val(), $("input#vegetable").val(), $("input#food").val(), $("input#animal").val()];
 
+    var first = favoriteThings[0];
+    var second = favoriteThings[1];
+    var third = favoriteThings[2];
+
+    var excerptFavoriteThings = [];
+    excerptFavoriteThings.push(second);
+    excerptFavoriteThings.push(first);
+    excerptFavoriteThings.push(third);
+
     $("#output").show();
-    $("p.output").text(favoriteThings);
-  })
-})
+    $("ul.output").append("<li>" + excerptFavoriteThings[0] + "</li>").append("<li>" + excerptFavoriteThings[1] + "</li>").append("<li>" + excerptFavoriteThings[2] + "</li>");
+  });
+});
